a6bc4765cf68227c0b1cbbc59a5b66d538b7a8e5
Gunner_Bills <billsg56@yahoo.com>
2018-12-03 17:10:31 -0500

    fix crash on Android. Fix various warnings on Android.

--- pages/AdvancedSearch.js
+++ pages/AdvancedSearch.js
@@ -27,25 +27,25 @@ export default class AdvancedSearch extends React.Component {
         </View>
 
         <View style={{height: 150, flexDirection: 'row', paddingTop:20,  alignItems: 'center'}}>
-          <View flex='5'>
+          <View style={{flex:5}} >
             <Text>Tags: </Text>
           </View>
-          <View flex='5'>
-            <Picker style={color='#ccc'}>
-              <Picker.item label="18+" value="18+" />
-              <Picker.item label="Alcohol-free" value="Alcohol-free" />
+          <View style={{flex:5}}>
+            <Picker>
+              <Picker.Item label="18+" value="18+" />
+              <Picker.Item label="Alcohol-free" value="Alcohol-free" />
             </Picker>
           </View>
         </View>
 
         <View style={{height: 150, flexDirection: 'row', paddingTop:20,  alignItems: 'center'}}>
-          <View flex='5'>
+          <View style={{flex:5}}>
             <Text>Category </Text>
           </View>
-          <View flex='5'>
+          <View style={{flex:5}}>
             <Picker>
-              <Picker.item label="Music" value="music" />
-              <Picker.item label="Food" value="food" />
+              <Picker.Item label="Music" value="music" />
+              <Picker.Item label="Food" value="food" />
             </Picker>
           </View>
         </View>
