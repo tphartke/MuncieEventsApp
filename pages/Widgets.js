9ccae887a6a92dcaca8d7d5ea280d98252d1f8d5
AryaKnight <mbratton@bsu.edu>
2019-04-04 13:43:57 -0400

    Chaned the widgets page to use api data

--- pages/Widgets.js
+++ pages/Widgets.js
@@ -1,71 +1,99 @@
 import React from 'react';
-import {Linking, Text, View, TextInput} from 'react-native';
+import {Text, View, WebView, TextInput} from 'react-native';
 import Styles from './Styles';
-import CustomButton from "./CustomButton";
+import CustomButton from './CustomButton'
 import Icon from 'react-native-vector-icons/Ionicons'
 import * as Animatable from 'react-native-animatable'
 import EventList from '../EventList'
+import APICacher from '../APICacher';
+import LoadingScreen from '../components/LoadingScreen';
+
 
 export default class Widgets extends React.Component {
   constructor(props){
-    super(props)
-    this.state = ({url: "",
-                   text: ""})
+    super(props);
+    this.state ={ isLoading: true,                
+                  url: "",
+                  text: ""}
+    this._getCachedDataAsync = this._getCachedDataAsync.bind(this);
+    this.APICacher = new APICacher();
   }
 
-render() {
-    widgetsView = null
+  componentDidMount(){
+    this._getCachedDataAsync();
+  }
+
+  render() {
+    widgetsTitle = "";
+    widgetsHTML = "";
+    webView = null
     searchView = null
-    if(this.state.url){
-        searchView = this.getSearchView()
+    topBar = this.getTopBar();
+    if(this.state.isLoading){
+      mainView = this.getLoadingScreen();
+    }
+    else if(this.state.url){
+      mainView = this.getSearchView();
     }
     else{
-        widgetsView = this.getWidgetsView()
+      widgetsTitle = this.state.dataSource.attributes.title;
+      widgetsHTML = this.state.dataSource.attributes.body;
+      mainView = this.getWebView(widgetsHTML)
     }
-  return (
-    <View style={Styles.wrapper}>
-       <View style={Styles.topBarWrapper}>
-        <Animatable.View animation = "slideInRight" duration={500} style={Styles.topBarContent}>
-            <CustomButton
-                text="Menu"
-                onPress={() => this.props.navigation.openDrawer()}/>
-            <TextInput
-                placeholder=' Search'
-                value={this.state.text} 
-                style={Styles.searchBar}
-                onChangeText={(text) => this.setState({text})}
-                onBlur={() => this.setState({url:'https://api.muncieevents.com/v1/events/search?q=' + this.state.text +  '&apikey=3lC1cqrEx0QG8nJUBySDxIAUdbvHJiH1'})}
-                showLoading='true'
-              />
-            <Icon name="ios-search" style={Styles.iosSearch}/>
-          </Animatable.View>
-        </View>
-      {searchView}
-      {widgetsView}
-    </View>
-  )
-}
-
-getWidgetsView(){
-    return(
-        <View>
-            <Text style={Styles.title}>
-                Widgets
-            </Text>
-            <Text style ={Styles.widgetContent}>Have a website? Support your community by adding a Muncie Events widget to it and keeping your visitors informed about local events. {"\n"}
-              To learn more about useing our widgets, just visit our{" "}  
-              <Text style={{color: 'blue'}}
-                onPress={() => Linking.openURL('https://muncieevents.com/widget')
-              }>
-               webpage.
-              </Text>
-            </Text>
-        </View>
+    return (
+      <View style={Styles.wrapper}>
+        {topBar}
+        <Text style ={Styles.title}> Widgets </Text>
+        {mainView}
+      </View>
     )
-}
+  }
 
+  getLoadingScreen(){
+    return(
+      <View>
+        <LoadingScreen/>
+      </View>
+    );
+  }
 
-getSearchView(){
+  getTopBar(){
+    return(
+          <View style={Styles.topBarWrapper}>
+            <Animatable.View animation = "slideInRight" duration={500} style={Styles.topBarContent}>
+                <CustomButton
+                    text="Menu"
+                    onPress={() => this.props.navigation.openDrawer()}/>
+                <TextInput
+                    placeholder=' Search'
+                    value={this.state.text} 
+                    style={Styles.searchBar}
+                    onChangeText={(text) => this.setState({text})}
+                    onBlur={() => this.setState({url:'https://api.muncieevents.com/v1/events/search?q=' + this.state.text +  '&apikey=3lC1cqrEx0QG8nJUBySDxIAUdbvHJiH1'})}
+                    showLoading='true'
+                  />
+                <Icon name="ios-search" style={Styles.iosSearch}/>
+              </Animatable.View>
+          </View>
+    );
+    
+  }
+
+  async _getCachedDataAsync(){
+    key = "WidgetsData"
+    url = "https://api.muncieevents.com/v1/pages/widgets?apikey=E7pQZbKGtPcOmKb6ednrQABtnW7vcGqJ"
+    hasAPIData = await this.APICacher._hasAPIData(key)
+    if(hasAPIData){
+      await this.APICacher._refreshJSONFromStorage(key, url)
+    }
+    else{
+        await this.APICacher._cacheJSONFromAPIWithExpDate(key, url)
+    }
+    await this.APICacher._getJSONFromStorage(key)
+      .then((response) => this.setState({dataSource: response, isLoading: false}))
+  }
+
+  getSearchView(){
     return(
       <View>
         <CustomButton 
@@ -76,6 +104,17 @@ getSearchView(){
         />
         <EventList apicall={this.state.url} />
       </View>
+    )
+  }
+
+  getWebView(html){
+    return(
+      <WebView
+        originWhitelist={['*']}
+        source={{ html: html }}
+        scrollEnabled={true}
+        startInLoadingState={false}
+      />
     )
   }
 }
