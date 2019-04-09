649062680dbc13c32d6402f7990ac0e0bec0a8ec
Gunner_Bills <gnbills@bsu.edu>
2019-04-09 12:56:19 -0400

    Remove unnecessary console log.

--- pages/Home.js
+++ pages/Home.js
@@ -26,7 +26,6 @@ export default class HomeScreen extends React.Component{
       }
 
       render(){
-        console.log("We're getting close")
         if(this.state.isLoading){
           mainView = this.getLoadingScreen();
         }
